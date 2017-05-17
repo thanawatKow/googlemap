e8cd4a29acc985af717490a43a2de9004b2b5aee
Thanawat <thanawat_ben@hotmail.com>
2017-05-17 11:38:12 +0700

    -

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -2,10 +2,32 @@
 
 class Welcome extends CI_Controller {
 
+	function __construct()
+	{
+	parent::__construct();
+	$this->load->library('googlemaps');
 
+	}
 	public function index()
 	{
-		$this->load->view('index');
+		$config = array();
+$config['center'] = 'auto';
+$config['onboundschanged'] = 'if (!centreGot) {
+	var mapCentre = map.getCenter();
+	marker_0.setOptions({
+		position: new google.maps.LatLng(mapCentre.lat(), mapCentre.lng())
+	});
+}
+centreGot = true;';
+$this->googlemaps->initialize($config);
+
+// set up the marker ready for positioning
+// once we know the users location
+$marker = array();
+$this->googlemaps->add_marker($marker);
+$data['map'] = $this->googlemaps->create_map();
+
+		$this->load->view('index',$data);
 	}
 }
 
